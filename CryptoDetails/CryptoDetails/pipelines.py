cb373c53487bad3d753882f9ff00d6494ddb5147
LegionAnkit <LegionAnkit1206>
2021-05-21 23:02:35 +0530

    Added MongoDb pipeline script

--- CryptoDetails/CryptoDetails/pipelines.py
+++ CryptoDetails/CryptoDetails/pipelines.py
@@ -1,13 +1,21 @@
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+# See: c
 
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymongo
 
 
-class CryptodetailsPipeline:
-    def process_item(self, item, spider):
-        return item
+class CryptodetailsPipeline(object):
+
+	def __init__(self):
+		self.conn = pymongo.MongoClient('localhost',27017)
+		db = self.conn['Crypto']
+		self.collection=db["crypto_details"]
+
+	def process_item(self, item, spider):
+		self.collection.insert(dict(item))
+		return item
